be9679e5f4433ff7ef6b2381523e8e44743d805c
eggman89 <official.snehasis@gmail.com>
2015-12-03 13:54:05 -0500

    Implemented Bayes
    Commit

--- build.sbt
+++ build.sbt
@@ -1,8 +1,8 @@
-name := "MusicRecomendationUsingSpark"
+name := "musicrecomendationusingspark"
 
 version := "1.0"
 
 scalaVersion := "2.10.4"
 
 libraryDependencies ++= Seq(
-  "org.apache.mahout" % "mahout-core" % "0.11" )
+ )
--- src/main/scala-2.10/profile.scala
+++ src/main/scala-2.10/profile.scala
@@ -13,7 +13,9 @@ import scala.collection.immutable.ListMap
 object profile {
   def get_existing(sqlContext:SQLContext, profile_id:String): DataFrame=
   {
+
     var sqlQuery = "SELECT * FROM meta_table JOIN triplets_table ON  meta_table.song_id = triplets_table.song_id WHERE triplets_table.user = '" +profile_id +"' ORDER BY triplets_table.play_count DESC"
+
     sqlContext.sql(sqlQuery)
   }
 
